6d4d518c33e13769fac07fcf9225c5538d760b40
Yi Zhu <zanydruid528@gmail.com>
2017-05-18 16:26:00 -0400

    enable eureka registration.

--- src/main/java/maps/MapsApp.java
+++ src/main/java/maps/MapsApp.java
@@ -2,11 +2,13 @@ package maps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by zany on 5/14/17.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MapsApp {
 
     public static void main(String[] args) {
